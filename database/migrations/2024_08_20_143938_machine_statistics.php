ef0c531c4d7675e7a3276494ebe0a91eccf48ed5
AliM-1997 <aliimortada797@gmail.com>
2024-08-20 17:53:53 +0300

    adding another attributes for the machine Statistics table

--- database/migrations/2024_08_20_143938_machine_statistics.php
+++ database/migrations/2024_08_20_143938_machine_statistics.php
@@ -15,7 +15,16 @@ return new class extends Migration
             $table->id();
             $table->string("machine_name");
             $table->foreign("machine_name")->references("name")->on("machines")->onUpdate("cascade")->onDelete("cascade");
-            
+            $table->decimal("operational_hours",10,2);
+            $table->decimal("MTTR",10,2);
+            $table->decimal("MTTD",10,2);
+            $table->decimal("MTBF",10,2);
+            $table->decimal("upTime",10,2);
+            $table->decimal("downTime",10,2);
+            $table->decimal("efficiency",10,2);
+            $table->decimal("availability",10,2);
+            $table->date("date");
+            $table->timestamps();
         });
     }
 
