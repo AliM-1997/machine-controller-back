8f91041faeb58305e6d378118a815d6135945e09
AliM-1997 <aliimortada797@gmail.com>
2024-08-21 16:13:50 +0300

    creating machineInput controller

---
+++ app/Http/Controllers/MachineInputController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MachineInputController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreallRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(all $all)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateallRequest $request, all $all)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(all $all)
+    {
+        //
+    }
+}
