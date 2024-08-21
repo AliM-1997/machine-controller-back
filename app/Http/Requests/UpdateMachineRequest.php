e7e04fe7b3130ecd2d589ebf68fb1e742fe25224
AliM-1997 <aliimortada797@gmail.com>
2024-08-21 22:03:44 +0300

    Adding validation rules to UpdateMachineRequest for machine

--- app/Http/Requests/UpdateMachineRequest.php
+++ app/Http/Requests/UpdateMachineRequest.php
@@ -11,7 +11,7 @@ class UpdateMachineRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,14 @@ class UpdateMachineRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'sometimes|required|string|max:255',
+            'serial_number' =>'sometimes|required|string|max:255|unique',
+            'status' => 'sometimes|required|in:active,under maintenance,attention',
+            'location' => 'nullable|string|max:255',
+            'image_path' => 'nullable|string|max:255',
+            'description' => 'nullable|string',
+            'last_maintenance' => 'sometimes|date',
+            'unit_per_hour' => 'sometimes|integer|min:0',
         ];
     }
 }
