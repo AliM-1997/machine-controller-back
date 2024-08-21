912b98651da74f7f8c2d11714b15adffc78bedb2
AliM-1997 <aliimortada797@gmail.com>
2024-08-21 15:33:14 +0300

    adding the fillable array for the machineInput model

--- app/Models/MachineInput.php
+++ app/Models/MachineInput.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MachineInput extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'machine_id',
+        'operating_time',
+        'downtime',
+        'number_of_failures',
+        'actual_output'
+    ];
 }
