505df37e81d9c23bc71cd5bfeaa9914d16c2901c
AliM-1997 <aliimortada797@gmail.com>
2024-08-21 23:40:19 +0300

    Adding index method to SparePartController for listing all sparePart

--- app/Http/Controllers/SparePartController.php
+++ app/Http/Controllers/SparePartController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SparePart;
 use Illuminate\Http\Request;
 
 class SparePartController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $sparePart=SparePart::all();
+        return response()->json([
+            "machineInputs"=>$sparePart
+        ]);
+    }
 }
