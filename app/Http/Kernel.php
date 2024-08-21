bc5860d8380cd935b9d6d558c36a523f1e50ad17
AliM-1997 <aliimortada797@gmail.com>
2024-08-22 02:09:46 +0300

    adding the UserMiddleware to the kernel aliases

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
+use App\Http\Middleware\UserMiddleware;
 class Kernel extends HttpKernel
 {
     /**
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
+        'auth.user' => UserMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
