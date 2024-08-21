b52246fa6ce019f5791b7251496104645f7493c4
AliM-1997 <aliimortada797@gmail.com>
2024-08-22 02:09:05 +0300

    adding the middleware for the user

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        // Check if user is authenticated
+        if (!$user) {
+            return response()->json([
+                "message" => "unauthorized",
+                "details" => "User not authenticated"
+            ], 401);
+        }
+
+        if ($user->role != "user") {
+            return response()->json([
+                "message" => "unauthorized",
+                "details" => "User does not have the required role"
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
