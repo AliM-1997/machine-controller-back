e01e2f498aa1017ee478f072884e5cafb4450be6
AliM-1997 <aliimortada797@gmail.com>
2024-08-23 01:43:28 +0300

    adding the validation rules to  update request for machine statistics

--- app/Http/Requests/UpdateMachineStatisticRequest.php
+++ app/Http/Requests/UpdateMachineStatisticRequest.php
@@ -11,7 +11,7 @@ class UpdateMachineStatisticRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,13 @@ class UpdateMachineStatisticRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'machine_id' => 'sometimes|required|exists:machines,id',
+            'MTTR' => 'sometimes|numeric|min:0',
+            'MTBF' => 'sometimes|numeric|min:0',
+            'availability' => 'sometimes|numeric|min:0|max:1',
+            'upTime' => 'sometimes|numeric|min:0',
+            'efficiency' => 'sometimes|numeric|min:0|max:100',
+            'date' => 'sometimes|date',
         ];
     }
 }
