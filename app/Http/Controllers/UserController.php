10ba4784eebcbc2a4bf072a8b6643eaf227efe97
AliM-1997 <aliimortada797@gmail.com>
2024-08-22 14:27:47 +0300

    adding show  method to user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,9 +29,9 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        return response()->json(["user"=>$user]);
     }
 
     /**
