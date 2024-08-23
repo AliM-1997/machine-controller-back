1d24b9566c8a012b4b57ee51400d3075540e6fa4
AliM-1997 <aliimortada797@gmail.com>
2024-08-23 15:45:01 +0300

    adding the task notification to database method

--- app/Notifications/TaskNotification.php
+++ app/Notifications/TaskNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\Task;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -10,13 +11,13 @@ use Illuminate\Notifications\Notification;
 class TaskNotification extends Notification
 {
     use Queueable;
-
+    public $task;
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(Task $task)
     {
-        //
+        $this->task = $task;
     }
 
     /**
@@ -26,29 +27,24 @@ class TaskNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
 
     /**
      * Get the array representation of the notification.
      *
      * @return array<string, mixed>
      */
-    public function toArray(object $notifiable): array
+    public function toArray($notifiable): array
     {
         return [
-            //
+            'task_id' => $this->task->id,
+            'jobDescription' => $this->task->jobDescription,
+            'assignedDate' => $this->task->assignedDate,
+            'dueDate' => $this->task->dueDate,
+            'location' => $this->task->location,
+            'status' => $this->task->status,
         ];
     }
 }
