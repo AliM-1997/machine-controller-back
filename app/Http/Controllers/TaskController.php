5f71c5d09ba6647035c126e38991e1701244f53f
AliM-1997 <aliimortada797@gmail.com>
2024-08-23 15:52:36 +0300

    adding index method to task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -11,7 +12,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $task=Task::all();
+        return response()->json(['task'=>$task]);
     }
 
     /**
