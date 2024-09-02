0452631a2a6f436920b6b05e4e10d2ca41a06c68
AliM-1997 <aliimortada797@gmail.com>
2024-09-02 21:56:05 +0300

    updating the nofification functions in notification controller

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,15 +1,35 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class NotificationController extends Controller
 {
+    /**
+     * Get all notifications for the authenticated user.
+     * 
+     *
+     * @return JsonResponse
+     */
+    public function getAllNotifications()
+    {
+        $user = Auth::user();
+        $notifications = $user->notifications;
+
+        return response()->json(['notifications' => $notifications], 200);
+    }
+
+    /**
+     * Mark a notification as read.
+     *
+     * @param int $notificationId
+     * @return JsonResponse
+     */
     public function markNotificationAsRead($notificationId)
     {
         $user = Auth::user();
-        $notification = $user->notifications->find($notificationId);
+        $notification = $user->notifications()->find($notificationId);
 
         if ($notification) {
             $notification->markAsRead();
@@ -19,14 +39,11 @@ class NotificationController extends Controller
         return response()->json(['message' => 'Notification not found.'], 404);
     }
 
-    public function getAllNotifications()
-    {
-        $user = Auth::user();
-        $notifications = $user->notifications;
-
-        return response()->json(['notifications' => $notifications], 200);
-    }
-
+    /**
+     * Get unread notifications for the authenticated user.
+     *
+     * @return JsonResponse
+     */
     public function getUnreadNotifications()
     {
         $user = Auth::user();
