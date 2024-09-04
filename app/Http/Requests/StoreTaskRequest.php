2cb4983a6814d2802249172c642bb2bbeaa84418
AliM-1997 <aliimortada797@gmail.com>
2024-09-04 18:33:18 +0300

    updating the validator method for the task create

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -22,9 +22,12 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_id' => 'required|exists:users,id',
-            'machine_id' => 'required|exists:machines,id',
-            'sparePart_id' => 'nullable|exists:spare_parts,id',
+            // 'user_id' => 'required|exists:users,id',
+            // 'machine_id' => 'required|exists:machines,id',
+            // 'sparePart_id' => 'nullable|exists:spare_parts,id',
+            'username' => 'required|exists:users,username',
+            'machine_serial_number' => 'required|exists:machines,serial_number',
+            'spare_Part_serial_number' => 'nullable|exists:spare_parts,serial_number',
             'jobDescription' => 'required|string',
             'assignedDate' => 'required|date',
             'dueDate' => 'required|date',
