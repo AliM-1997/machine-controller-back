cb5f23785c88c8ec95a4bfe1dac4c83cbc565993
AliM-1997 <aliimortada797@gmail.com>
2024-09-13 23:50:41 +0300

    updating sparepart model

--- app/Models/sparePart.php
+++ app/Models/sparePart.php
@@ -16,9 +16,15 @@ class SparePart extends Model
         'description',
         'image_path',
         'type',
+        'standard_pressure',
+        'life_cycle',
     ];
     public function task()
     {
         return $this->hasMany(Task::class);
+    }
+    public function machineSpareParts()
+    {
+        return $this->hasMany(MachineSparePart::class, 'spare_part_serial_number', 'serial_number');
     }
 }
