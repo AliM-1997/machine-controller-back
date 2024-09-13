a5ecbea2847f2731bdeecb0c796b15adda635def
AliM-1997 <aliimortada797@gmail.com>
2024-09-13 23:51:07 +0300

    updating factories

--- database/factories/SparePartFactory.php
+++ database/factories/SparePartFactory.php
@@ -23,6 +23,8 @@ class SparePartFactory extends Factory
             'description' => fake()->optional()->text(),
             'image_path' => 'path/to/image.jpg',
             'type'=>fake()->randomElement(['Mechanical', 'Electrical', 'Oil']),
+            'standard_pressure' => $this->faker->randomFloat(2, 1, 100), 
+            'life_cycle' => fake()->numberBetween(0, 1000),
         ];
     }
 }
