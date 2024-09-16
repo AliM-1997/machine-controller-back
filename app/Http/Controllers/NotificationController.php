5825377f5b5f7a3d02b29242d2ebc4c9ae035443
AliM-1997 <aliimortada797@gmail.com>
2024-09-16 15:06:20 +0300

    adding the listen to notification creating to handle online request

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Events\Notifications;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
 
@@ -28,16 +30,31 @@ class NotificationController extends Controller
      */
     public function markNotificationAsRead($notificationId)
     {
+        // Get the authenticated user
         $user = Auth::user();
+        
+        // Find the notification belonging to the user
         $notification = $user->notifications()->find($notificationId);
-
+    
+        // Check if the notification exists
         if ($notification) {
+            // Mark the notification as read
             $notification->markAsRead();
+    
+            // Optionally get the unread notifications count
+            // $unreadCount = $user->unreadNotifications->count();
+    
+            // Optionally broadcast the notification update event
+            broadcast(new Notifications($user->name, $notification->id));
+    
+            // Return a success response
             return response()->json(['message' => 'Notification marked as read.'], 200);
         }
-
+    
+        // Return an error if the notification is not found
         return response()->json(['message' => 'Notification not found.'], 404);
     }
+    
 
     /**
      * Get unread notifications for the authenticated user.
@@ -48,7 +65,21 @@ class NotificationController extends Controller
     {
         $user = Auth::user();
         $unreadNotifications = $user->unreadNotifications;
+        $unreadCount = $user->unreadNotifications->count();
 
-        return response()->json(['notifications' => $unreadNotifications], 200);
+        broadcast(new Notifications($user,$unreadNotifications));
+        return response()->json(['notifications' => $unreadNotifications , 'count'=>$unreadCount], 200);
+    }
+
+    public function notificationCount(Request $request)
+    {
+        $user = Auth::user();
+
+        $unreadCount = $user->unreadNotifications->count();
+
+        // broadcast(new Notifications($user,$notification));
+
+
+        return response()->json(['unreadCount' => $unreadCount], 200);
     }
 }
