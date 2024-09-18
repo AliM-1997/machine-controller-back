862ec1bb1a08cd2c097a32a870db02dab5629584
AliM-1997 <aliimortada797@gmail.com>
2024-09-18 20:39:52 +0300

    adding get last sensor data to sensor controller

--- app/Http/Controllers/SensorDataController.php
+++ app/Http/Controllers/SensorDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\SensorDataRecived;
 use Illuminate\Http\Request;
 use App\Models\SensorData;
 
@@ -18,6 +19,7 @@ class SensorDataController extends Controller
                 $sensorData->Temperature = $data['Temperature'];
                 $sensorData->save();
 
+            // event(new SensorDataRecived(["Temperature",$sensorData]));
                 return response()->json(['message' => 'Data received successfully'], 200)
                     ->header('Access-Control-Allow-Origin', '*')
                     ->header('Access-Control-Allow-Methods', 'POST')
@@ -29,4 +31,21 @@ class SensorDataController extends Controller
             return response()->json(['error' => 'Invalid data format'], 400);
         }
     }
+    public function getsensordata()
+    {
+        $lastSensorData = SensorData::orderBy('id', 'desc')->first(); 
+
+        if ($lastSensorData) {
+            return response()->json([
+                'success' => true,
+                'data' => $lastSensorData
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'No sensor data found'
+        ], 404);
+    }
+
 }
